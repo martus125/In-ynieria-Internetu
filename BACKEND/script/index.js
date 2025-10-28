2e2817b528b62fd4b0ba8c35726adf5122863aef
Marta <marciajdrs19@gmail.com>
2025-10-28 22:23:25 +0100

    polaczony sql mniej wiecej i front git jest bez podstron

---
+++ BACKEND/script/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.json("Test backend miumiu")
+})
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"123456789",
+    database:"uzytkownicy"
+})
+
+app.get("/", (req,res)=>{
+    res.json("Hello tutaj backend elo melo")
+})
+
+app.get("/autorzy", (req,res)=>{
+    const q = "SELECT * FROM uzytkownicy"
+    db.query(q, (err, res) =>{
+        if(err) return res.json(err)
+        return res.json(data)
+
+    })
+})
+app.listen(8800, () =>{
+
+    console.log("Connected supi brawo!!")
+})
